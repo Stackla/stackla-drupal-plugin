c9580341040b77ef9924936728c78a17fb6970b3
Phil Dodd <tripper54@gmail.com>
2015-04-16 11:27:43 +1000

    filter themeing

--- modules/stackla_field/templates/stackla_field.tpl.php
+++ modules/stackla_field/templates/stackla_field.tpl.php
@@ -10,12 +10,12 @@
 ?>
 <div class="stackla-field">
   Stackla field template.
-  <?php foreach ($filters as $delta => $filter): ?>
-  <?php if(!empty($filter['title'])): ?>
-  <div class="filter-<?php print $delta; ?> title">
-    Filter: <?php print $filter['title']; ?>
-  </div>
+  <?php if(!empty($filters)) : ?>
+  <div class="filter-label">Filters:</div>
+  <?php print(render($filters)); ?>
   <?php endif; ?>
-  <?php endforeach; ?>
+  <div class="stackla-widget">
+    Widget output here.
+  </div>
 </div>
 
