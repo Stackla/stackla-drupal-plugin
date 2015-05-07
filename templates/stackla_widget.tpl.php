35cf51a2711c870a077015ce55fe05adceb0d455
Phil Dodd <tripper54@gmail.com>
2015-05-07 13:28:02 +1000

    fixes #STTP-63

--- templates/stackla_widget.tpl.php
+++ templates/stackla_widget.tpl.php
@@ -10,7 +10,6 @@
  */
 ?>
 <div class="stackla">
-  <h3>Stackla widget: <?php print check_plain($data['name']); ?></h3>
   <?php if(!empty($filters)) : ?>
   <div class="filters">
   <?php print(render($filters)); ?>
