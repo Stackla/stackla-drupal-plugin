f3d0316b1d7210bc5ea25d78e5b61ccf1c057778
Helman Budianto <helman.budianto@stackla.com>
2015-11-24 14:34:17 +1100

    Added Default filter and remove filter creation

--- modules/stackla_field/js/stackla_field_widget.js
+++ modules/stackla_field/js/stackla_field_widget.js
@@ -5,9 +5,33 @@
 (function($) {
   Drupal.behaviors.stackla_field = {
     attach: function(context) {
-      var lookDetails = $('.look-details');
-      var lookSelected = $('input.look-options:checked').next().next();
-      lookDetails.insertAfter(lookSelected);
+      // var lookDetails = $('.look-details');
+      // var lookSelected = $('input.look-options:checked').next().next();
+      // lookDetails.insertAfter(lookSelected);
+      $('.look-options input[value=new]')
+        .parent().addClass('st-widgetType st-widgetType-new' + ($('.look-options input[value=new]').is(':checked') ? ' on' : ''));
+      $('.look-options input[value=derive]')
+        .parent().addClass('st-widgetType st-widgetType-derive' + ($('.look-options input[value=derive]').is(':checked') ? ' on' : ''));
+      $('.look-options input[value=clone]')
+        .parent().addClass('st-widgetType st-widgetType-clone' + ($('.look-options input[value=clone]').is(':checked') ? ' on' : ''));
+
+      $('.look-options input[type=radio]')
+        .change(function(e){
+          if ($(this).is(':checked')) {
+            $(this).parent().addClass('on');
+          } else {
+            $(this).parent().removeClass('on');
+          }
+        })
+      $('.look-details input[value=base_waterfall]').parent().addClass('st-widgetStyle st-widgetStyle-base_waterfall');
+      $('.look-details input[value=base_carousel]').parent().addClass('st-widgetStyle st-widgetStyle-base_carousel');
+      $('.look-details input[value=base_billboard]').parent().addClass('st-widgetStyle st-widgetStyle-base_billboard');
+      $('.look-details input[value=base_feed]').parent().addClass('st-widgetStyle st-widgetStyle-base_feed');
+      $('.look-details input[value=base_slideshow]').parent().addClass('st-widgetStyle st-widgetStyle-base_slideshow');
+      $('.look-details input[type=radio]')
+        .change(function(e){
+          $('.st-delete-old-widget').closest('.delete-options').removeClass('st-hide');
+        });
     }
   };
 })(jQuery);
